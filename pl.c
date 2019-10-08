6523a1aa603a77cc66bb18aa862e7aa63230fb93
bhawna1999 <32545043+bhawna1999@users.noreply.github.com>
2019-10-08 15:37:36 +0530

    Update pl.c

--- pl.c
+++ pl.c
@@ -2,8 +2,10 @@
 int main() {
 	//code
 	float sp ,cp,l,p;
-	printf("enter the value of cost price and selling price : ");
-	scanf("%f %f ",&cp,&sp);
+	printf("Enter the value of cost price  : ");
+	scanf("%f",&cp);
+	printf("enter the value of selling price : ");
+	scanf("%f",&sp);
 	if(sp<cp)
 	{
 	    l=cp-sp;
