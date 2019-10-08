e226e8adc83d3dfd9cabfc4dafcde03369605f45
bhawna1999 <32545043+bhawna1999@users.noreply.github.com>
2019-10-08 16:38:30 +0530

    Update pl.c
    in c for taking input in scanf() you need to add '&' symbol before the variable else itwill create errors by showing garbage value

--- pl.c
+++ pl.c
@@ -3,7 +3,7 @@ int main() {
 	//code
 	float sp ,cp,l,p;
 	printf("enter the value of cost price and selling price : ");
-	scanf("%f %f ",cp,sp);
+	scanf("%f %f ",&cp,&sp);
 	if(sp<cp)
 	{
 	    l=cp-sp;
